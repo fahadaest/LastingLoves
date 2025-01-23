f2824b7c4ee01c80371c505a51dbb23207169cf6
Fahad Nasir <fahadaestt@gmail.com>
2025-01-23 14:09:57 +0500

    home page structure done

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,24 +1,20 @@
-import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import MessagesOfLove from '../components/homePageSections/MessagesOfLove';
+import CherishYourLove from '../components/homePageSections/CherishYourLove';
+import PersonalVideoMessage from '../components/homePageSections/PersonalVideoMessage';
+import GetInTouch from '../components/homePageSections/GetInTouch';
 
 function Home() {
   return (
-    <div className="h-screen">
-      <section className=" bg-bg-green min-h-[90vh]">
+    <div className="h-screen hero-background-img" >
 
-        <div className="bg-slate-200 min-w-[100vw] min-h-[90vh] relative overflow-hidden">
-          <img
-            className="w-full h-[90vh] object-cover fixed z-0"
-            src="https://cdn.b12.io/client_media/cQFRBNdt/07f1ceb4-c694-11ef-9c5e-0242ac110002-jpg-hero_image.jpeg"
-          />
-          <div className="absolute z-10 inset-0 bg-gradient-to-t from-black/70 to-black/30"></div>
-        </div>
+      <MessagesOfLove />
+      <CherishYourLove />
+      <PersonalVideoMessage />
+      <GetInTouch />
 
-
-
-      </section>
-      <section className=" bg-pink-300 min-h-[80vh]  z-20 relative">
-      </section>
-    </div>
+    </div >
   );
 }
 
