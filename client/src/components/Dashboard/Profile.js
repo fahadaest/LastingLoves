38681f5cfae4dcec88d50ac75c7f3e8e7294e19d
Fahad Nasir <fahadaestt@gmail.com>
2025-03-25 02:47:34 +0500

    issues

--- client/src/components/Dashboard/Profile.js
+++ client/src/components/Dashboard/Profile.js
@@ -31,7 +31,7 @@ export default function Profile() {
 
     useEffect(() => {
         dispatch(checkAuthStatus());
-    }, [dispatch]);
+    }, []);
 
     React.useEffect(() => {
         if (profileData) {
