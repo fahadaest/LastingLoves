d272c574c9af1781d84f76b76b72d9d38af8bc31
Fahad <fahhadnasir@gmail.com>
2025-06-10 02:08:58 +0400

    testing again

--- server/models/User.js
+++ server/models/User.js
@@ -15,6 +15,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: function () {
             return !this.googleId;
+            return !this.appleId;
         },
     },
     googleId: {
